f54d8f30fd36e7849eee6f1066cc0f8e1d725fc0
MIN-HYEOKJEON <mari394337@gmail.com>
2024-11-25 14:43:23 +0900

    Design: PostItem

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
 import Nav from '@/components/Nav';
+import ClientLayout from './layout-client';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -32,8 +33,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Nav />
-        {children}
+        <ClientLayout>
+          <Nav />
+          {children}
+        </ClientLayout>
       </body>
     </html>
   );
---
+++ components/Post/PostList.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import PostItem from './PostItem';
+
+interface PostType {
+  amount: number;
+  authorNickName: string;
+  created_at: string;
+  gymName: string;
+  id: number;
+  imageUrl1: string;
+  postStatus: string;
+  title: string;
+  wishCount: number;
+}
+
+export default function PostList() {
+  const { data } = useQuery({
+    queryKey: ['post'],
+    queryFn: async () => (await axios.get('http://localhost:4000/posts')).data,
+    staleTime: 1000 * 10,
+  });
+
+  console.log(data);
+
+  return (
+    <div className=" flex flex-wrap gap-8 w-full">
+      {data &&
+        data.map((post: PostType) => <PostItem key={post.id} {...post} />)}
+    </div>
+  );
+}
