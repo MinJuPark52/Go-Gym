81e06ca6167b5a17ecc3a46b4f246ba2321e9122
MIN-HYEOKJEON <mari394337@gmail.com>
2024-11-26 09:12:01 +0900

    Design: footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from 'next/font/local';
 import './globals.css';
 import Nav from '@/components/Nav';
 import ClientLayout from './layout-client';
+import Footer from '@/components/Footer';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -36,6 +37,7 @@ export default function RootLayout({
         <ClientLayout>
           <Nav />
           {children}
+          <Footer />
         </ClientLayout>
       </body>
     </html>
