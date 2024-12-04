0036e9b10da610b47b7cea3e3273d213b4f857e0
MinJuPark52 <als4013@naver.com>
2024-12-04 15:52:02 +0900

    Fix:Api 연결

--- components/User/Signup.tsx
+++ components/User/Signup.tsx
@@ -49,8 +49,44 @@ const SignupInput: React.FC<InputProps> = ({
 );
 
 // 관심지역
-const areas = ["서울"];
-const areas2 = ["부산"];
+const areas = [
+  "서울특별시",
+  "부산광역시",
+  "대구광역시",
+  "인천광역시",
+  "광주광역시",
+  "대전광역시",
+  "울산광역시",
+  "세종특별자치시",
+  "경기도",
+  "충청북도",
+  "충청남도",
+  "전라남도",
+  "경상북도",
+  "경상남도",
+  "제주특별자치시",
+  "강원특별자치도",
+  "전북특별자치도",
+];
+const areas2 = [
+  "서울특별시",
+  "부산광역시",
+  "대구광역시",
+  "인천광역시",
+  "광주광역시",
+  "대전광역시",
+  "울산광역시",
+  "세종특별자치시",
+  "경기도",
+  "충청북도",
+  "충청남도",
+  "전라남도",
+  "경상북도",
+  "경상남도",
+  "제주특별자치시",
+  "강원특별자치도",
+  "전북특별자치도",
+];
 
 export default function SignupPage() {
   const [signupFormData, setsignupFormData] = useState({
@@ -340,7 +376,7 @@ export default function SignupPage() {
               onChange={handleSignupChange("area")}
               className="w-full p-3 border border-gray-300 rounded-md focus:outline-none"
             >
-              <option value="">관심지역</option>
+              <option value="">관심지역1</option>
               {areas.map((area) => (
                 <option key={area} value={area}>
                   {area}
