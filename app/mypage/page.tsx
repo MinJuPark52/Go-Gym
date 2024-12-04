80f52889a55924d2df00b76c19b8f41c3ce4ac65
전민혁 <mari394337@gmail.com>
2024-12-04 13:43:38 +0900

    Design: 마이페이지 레이아웃

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -7,6 +7,11 @@ export default function MyPage() {
       <div className=" flex flex-col w-[75%] gap-12">
         <ChangeProfile />
         <GympayBox />
+        <div>내가 작성한 게시글</div>
+        <div>내가 찜한 게시글</div>
+        <div>최근 본 게시글</div>
+        <div>구매 목록 조회</div>
+        <div>판매 목록 조회</div>
       </div>
     </div>
   );
