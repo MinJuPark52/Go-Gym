8f16b131e66df45921582a70a2209c22aafd9682
MinJuPark52 <als4013@naver.com>
2024-12-10 15:10:17 +0900

    Fix:Login 홈 이동

--- next.config.ts
+++ next.config.ts
@@ -9,8 +9,7 @@ const nextConfig: NextConfig = {
       },
       {
         source: "/backend/:path*", // Local API
-        destination:
-          "https://7eae-2001-2d8-2181-6016-70a4-4d95-c733-9b05.ngrok-free.app/:path*",
+        destination: "https://go-gym.site/:path*",
       },
       {
         source: "/chat/:path*", // Local API
