d4b59edac4ea7f4e5e52d613814e28c219caf263
전민혁 <mari394337@gmail.com>
2024-12-12 21:55:24 +0900

    수정

--- components/Post/PostList.tsx
+++ components/Post/PostList.tsx
@@ -21,13 +21,28 @@ export default function PostList({
   data,
   style,
 }: {
-  data: PostType[];
+  data?: PostType[];
   style?: string;
 }) {
+  const { data: defaultData, isPending } = useQuery({
+    queryKey: ["default"],
+    queryFn: async () => (await axios.get("http://localhost:4000/posts")).data,
+    staleTime: 10000,
+  });
+
+  console.log(defaultData);
+
+  if (!data && isPending) {
+    return <p>데이터 로딩중...</p>;
+  }
+
   return (
     <div className={`mb-12 flex w-full min-w-[750px] gap-8 ${style}`}>
-      {data &&
-        data.map((post: PostType) => <PostItem key={post.id} {...post} />)}
+      {data
+        ? data.map((post: PostType) => <PostItem key={post.id} {...post} />)
+        : defaultData.map((post: PostType) => (
+            <PostItem key={post.id} {...post} />
+          ))}
     </div>
   );
 }
